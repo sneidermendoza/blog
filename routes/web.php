23f928842165af10909fa5cfeaca4b9e2e68e24d
sneider mendoza <mariasol0304@gmail.com>
2022-03-21 12:13:40 -0500

    iniciando proyecto

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function (){
+    // return view('welcome');//
+    return "bienvenido a SMS";
+});
+
+Route::get('cursos', function (){
+    return "Bienvenido a la pagina cursos";
+});
+
+Route::get('cursos/crear', function (){
+    return "En esta pagina podras crear un curso";
+});
+
+
+// Route::get('cursos/{curso}', function ($curso){
+//     return "Bienvenido al curso: $curso";
+// });
+
+
+// esta ruta recibe varias variables en la funcion
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null){
+    if ($categoria){
+        return "Bienvenido al curso: $curso, de la categoria $categoria";
+    }else{
+        return "Bienvenido al curso: $curso";
+    }
+    
+});
